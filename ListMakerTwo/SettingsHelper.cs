813615c95f061672bd4fd2b1365525c3204ce8c1
Henrik <Henrik@Matrix.local>
2017-03-15 21:40:07 +0100

    Started to work next iteration

---
+++ ListMakerTwo/SettingsHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListMakerTwo
+{
+    public class SettingsHelper
+    {
+        public static void ReadSpan(string val, out int first_part, out int second_part)
+        {
+            var string_one = "";
+            var string_two = "";
+
+            ReadSpan(val, out string_one, out string_two);
+
+            first_part = int.Parse(string_one);
+            second_part = int.Parse(string_two);
+        }
+
+        public static void ReadSpan(string val, out string first_part, out string second_part)
+        {
+            first_part = val.Substring(0, val.IndexOf(":"));
+            second_part = val.Substring(val.IndexOf(":") + 1);
+        }
+    }
+}
---
+++ Tests/ListMakerTwo/SettingsHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ListMakerTwo;
+
+namespace Tests.ListMakerTwo
+{
+    [TestFixture]
+    public class SettingsHelperTests
+    {
+        [Test]
+        public void CanSplitString()
+        {
+            var part1 = "";
+            var part2 = "";
+            SettingsHelper.ReadSpan("Part1:Part2", out part1, out part2);
+
+            Assert.That(part1, Is.EqualTo("Part1"));
+            Assert.That(part2, Is.EqualTo("Part2"));
+        }
+
+        [Test]
+        public void CanSplitInt()
+        {
+            var part1 = 0;
+            var part2 = 0;
+            SettingsHelper.ReadSpan("111:222", out part1, out part2);
+
+            Assert.That(part1, Is.EqualTo(111));
+            Assert.That(part2, Is.EqualTo(222));
+        }
+    }
+}
