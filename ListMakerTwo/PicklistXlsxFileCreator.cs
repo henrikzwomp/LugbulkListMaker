a79b904f141a7ed6282e622fc75b3a24e586b7be
Henrik <Henrik@Matrix.local>
2017-04-05 20:13:05 +0200

    Completed PicklistXlsxFileCreator
    Now I just need to write the tests for it.

--- ListMakerTwo/PicklistXlsxFileCreator.cs
+++ ListMakerTwo/PicklistXlsxFileCreator.cs
@@ -17,60 +17,72 @@ namespace ListMakerTwo
             // Page one
             int page_one_buyers_row_start = 2;
             int page_one_buyers_row_end = 56;
-
-            work_sheet.Cell(1, "B").Value = picklist.ElementID;
-            work_sheet.Cell(2, "B").Value = picklist.BricklinkDescription;
-            work_sheet.Cell(5, "B").Value = picklist.BricklinkColor;
-            work_sheet.Cell(6, "B").Value = picklist.MaterialColor;
+            SetElementInfo(work_sheet, picklist, 1);
 
             for (int i = page_one_buyers_row_start; i <= page_one_buyers_row_end; i++)
             {
-                if (line_count < picklist.Reservations.Count)
-                {
-                    work_sheet.Cell(i, "E").Value = picklist.Reservations[line_count].Receiver.Id;
-                    work_sheet.Cell(i, "F").Value = picklist.Reservations[line_count].Receiver.Name;
-                    work_sheet.Cell(i, "G").Value = picklist.Reservations[line_count].Amount;
-                }
-                else
-                {
-                    work_sheet.Cell(i, "H").Delete(XLShiftDeletedCells.ShiftCellsLeft);
-                    work_sheet.Cell(i, "G").Delete(XLShiftDeletedCells.ShiftCellsLeft);
-                    work_sheet.Cell(i, "F").Delete(XLShiftDeletedCells.ShiftCellsLeft);
-                    work_sheet.Cell(i, "E").Delete(XLShiftDeletedCells.ShiftCellsLeft);
-                }
-
+                SetReservationLine(work_sheet, picklist, line_count, i);
                 line_count++;
             }
 
             // Page two
-            // ToDo: Refactor?
-            int page_two_start_after = 56;
+            int page_two_starts_at = 57;
+            int page_two_ends_at = 112;
             int page_two_buyers_row_start = 58;
             int page_two_buyers_row_end = 112;
+            int page_two_instruction_box_row = 56;
+
+            if (line_count >= picklist.Reservations.Count)
+            {
+                DeletePageTwoElementInfo(work_sheet, 
+                    page_two_starts_at, page_two_ends_at, 
+                    page_two_instruction_box_row);
+            }
+            else
+            {
+                SetElementInfo(work_sheet, picklist, page_two_starts_at);
+            }
 
             for (int i = page_two_buyers_row_start; i <= page_two_buyers_row_end; i++)
             {
-                if (line_count < picklist.Reservations.Count)
-                {
-                    work_sheet.Cell(i, "E").Value = picklist.Reservations[line_count].Receiver.Id;
-                    work_sheet.Cell(i, "F").Value = picklist.Reservations[line_count].Receiver.Name;
-                    work_sheet.Cell(i, "G").Value = picklist.Reservations[line_count].Amount;
-                }
-                else
-                {
-                    work_sheet.Cell(i, "H").Delete(XLShiftDeletedCells.ShiftCellsLeft);
-                    work_sheet.Cell(i, "G").Delete(XLShiftDeletedCells.ShiftCellsLeft);
-                    work_sheet.Cell(i, "F").Delete(XLShiftDeletedCells.ShiftCellsLeft);
-                    work_sheet.Cell(i, "E").Delete(XLShiftDeletedCells.ShiftCellsLeft);
-                }
-
+                SetReservationLine(work_sheet, picklist, line_count, i);
                 line_count++;
             }
+        }
 
-            work_sheet.Cell(1 + page_two_start_after, "B").Value = picklist.ElementID;
-            work_sheet.Cell(2 + page_two_start_after, "B").Value = picklist.BricklinkDescription;
-            work_sheet.Cell(5 + page_two_start_after, "B").Value = picklist.BricklinkColor;
-            work_sheet.Cell(6 + page_two_start_after, "B").Value = picklist.MaterialColor;
+        private static void DeletePageTwoElementInfo(IXLWorksheet work_sheet, 
+            int page_two_start_at, int page_two_ends_at, int page_two_instruction_box_row)
+        {
+            work_sheet.Rows(page_two_start_at, page_two_ends_at).Delete();
+
+            work_sheet.MergedRanges.Where(x => x.FirstCell().Address.RowNumber == page_two_instruction_box_row).First()
+                .Delete(XLShiftDeletedCells.ShiftCellsLeft);
+        }
+
+        private static void SetElementInfo(IXLWorksheet work_sheet, LugBulkPicklist picklist, int origin_row)
+        {
+            work_sheet.Cell(origin_row + 0, "B").Value = picklist.ElementID;
+            work_sheet.Cell(origin_row + 1, "B").Value = picklist.BricklinkDescription;
+            work_sheet.Cell(origin_row + 4, "B").Value = picklist.BricklinkColor;
+            work_sheet.Cell(origin_row + 5, "B").Value = picklist.MaterialColor;
+        }
+
+        private static void SetReservationLine(IXLWorksheet work_sheet, LugBulkPicklist picklist, 
+            int line_count, int current_line)
+        {
+            if (line_count < picklist.Reservations.Count)
+            {
+                work_sheet.Cell(current_line, "E").Value = picklist.Reservations[line_count].Receiver.Id;
+                work_sheet.Cell(current_line, "F").Value = picklist.Reservations[line_count].Receiver.Name;
+                work_sheet.Cell(current_line, "G").Value = picklist.Reservations[line_count].Amount;
+            }
+            else
+            {
+                work_sheet.Cell(current_line, "H").Delete(XLShiftDeletedCells.ShiftCellsLeft);
+                work_sheet.Cell(current_line, "G").Delete(XLShiftDeletedCells.ShiftCellsLeft);
+                work_sheet.Cell(current_line, "F").Delete(XLShiftDeletedCells.ShiftCellsLeft);
+                work_sheet.Cell(current_line, "E").Delete(XLShiftDeletedCells.ShiftCellsLeft);
+            }
         }
     }
 }
